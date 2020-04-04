d411c11b941775957da2c1dac2f736ae1ffb13ae
zacjord <zjordan@ufl.edu>
2020-04-04 20:45:24 +0000

    first draft

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '70741_kanye-west-png(1).png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Zachary Jordan
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+    
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
