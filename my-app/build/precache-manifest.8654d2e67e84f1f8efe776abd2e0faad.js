5bcc3e390b4290066915671a810a1f0804060d06
zacjord <zjordan@ufl.edu>
2020-04-05 01:04:48 +0000

    edit3

---
+++ my-app/build/precache-manifest.8654d2e67e84f1f8efe776abd2e0faad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83734141ba784fed34ef603ac6b8eb70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d34e360a298895448d",
+    "url": "/static/css/main.6e4f514d.chunk.css"
+  },
+  {
+    "revision": "6210bf1ded25448980ff",
+    "url": "/static/js/2.2594f893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2594f893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d34e360a298895448d",
+    "url": "/static/js/main.77b2ca95.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "6f54178b758377b2da2186f12f21c74f",
+    "url": "/static/media/kanye.6f54178b.png"
+  }
+]);
